9074c0d665b041ffc16ae1227e14ac978a87886c
Kenrick Guie <nyhunter77@yahoo.com>
2017-09-23 10:49:02 -0400

    Initial Commit

---
+++ world tracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  world tracking
+//
+//  Created by Kenrick Guie on 9/23/17.
+//  Copyright © 2017 Kenrick Guie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
